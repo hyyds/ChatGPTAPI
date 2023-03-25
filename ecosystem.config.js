fb48775a0647cb83142aa0ebee949c1d2f453917
hc <513416443@qq.com>
2023-03-25 15:14:46 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "ChatGPTAPI",
       script: "ts-node",
+      cron_restart: '1 1 * * *',
       args: '-P ./tsconfig.build.json ./src/main.ts',
       log_date_format: "YYYY-MM-DD HH:mm Z",
     },
